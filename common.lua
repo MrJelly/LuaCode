9161c534b95634025c6797b52c91264f0f637b39
jelly <20690895@qqo.com>
2017-02-18 00:23:51 +0800

    update

--- common.lua
+++ common.lua
@@ -1,2 +1,17 @@
-local xx = 88
-print(xx)
+local Messager = {}}
+Messager.__index = Messager
+
+function Messager.new( ... )
+    local o = {}
+    setmetatable(o, Messager)
+    return o
+end
+
+function Messager.Run(func, ...)
+    self.co = coroutine.create( func )
+    local ok, err_msg = pcall(coroutine.resume( self.co, ... ))
+    if ok then
+        error(err_msg)
+    end
+    return coroutine.yield()
+end
